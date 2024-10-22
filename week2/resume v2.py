474670a277fad35114998c1bd3e89f496aeeb872
nuengtawan-tnk <67319010040@tnk.ac.th>
2024-10-22 16:29:03 +0700

    resume v2
    dadasjjjjcneicn kecwelkmc lmcelmlmce

---
+++ week2/resume v2.py
@@ -0,0 +1,13 @@
+name = input("กรุณาป้อนชื่อจริง: ")
+student_id = input("กรุณาป้อนรหัสประจำตัวนักศึกษา: ")
+year = input("กรุณาป้อนชั้นปี: ")
+nickname = input("กรุณาป้อนชื่อเล่น: ")
+height = float(input("กรุณาป้อนส่วนสูง (เมตร): "))
+weight = float(input("กรุณาป้อนน้ำหนัก (กิโลกรัม): "))
+total = height + weight
+
+print(f"ชื่อ : {name} อายุ {age}   ปี")
+print(f'รหัสประจำตัวนักศึกษา : {id} ระดับชั้น : {Year}')
+print(f'ชื่อเล่น {NickName}')
+print(f"ส่วนสูง : {Height}  น้ำหนัก {W}  กิโลกรัม" )
+print(f'ส่วนสูง + น้ำหนัก =  {Value}')
